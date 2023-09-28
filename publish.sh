a135ab79a8a75003bc89f4348531bbdeccd0904a
Mike Caron <mike.caron@patientpop.com>
2023-09-28 02:48:19 -0400

    also delete files during publish

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd dist
-aws s3 sync --exclude "*.html" --cache-control "public, max-age=604800, immutable" . s3://mcaron-aphints-website
-aws s3 sync --include "*.html" --cache-control no-cache --content-type text/html . s3://mcaron-aphints-website
+aws s3 sync --exclude "*.html" --cache-control "public, max-age=604800, immutable" --delete . s3://mcaron-aphints-website
+aws s3 sync --include "*.html" --cache-control no-cache --content-type text/html --delete . s3://mcaron-aphints-website
 cd ..
